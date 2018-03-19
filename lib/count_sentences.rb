659e0da3cf168153f6564b12945cb0c3a2883c16
Rani Shah <rani76@gmail.com>
2018-03-19 01:03:46 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,21 +1,6 @@
+
 require 'pry'
 
 class String
-  def sentence?
-    self.end_with?(".")
-    end
 
-  def question?
-   self.end_with?("?")
-end
-
-  def exclamation?
-    self.end_with?("!")
-  end
-
-  def count_sentences
-    arr = self.split(/[.!?]/).delete_if {|w| w.size < 2}
-    return arr.count
-end
-
-end
+end 
